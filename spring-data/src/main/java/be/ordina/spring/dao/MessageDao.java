70aed203e0ae4440fc0158dec52af6fd37705092
Ken Coenen <ken.coenen@ordina.be>
2015-08-19 09:40:52 +0200

    Added NamedParameterJdbcTemplate example

--- spring-data/src/main/java/be/ordina/spring/dao/MessageDao.java
+++ spring-data/src/main/java/be/ordina/spring/dao/MessageDao.java
@@ -2,11 +2,17 @@ package be.ordina.spring.dao;
 
 import be.ordina.spring.domain.Message;
 
+import java.util.List;
+
 /**
  * @author Ken Coenen
  */
 public interface MessageDao {
+    public List<Message> getMessages();
+
     public Message getMessage(Long id);
 
     public int getCount();
+
+    public List<Message> searchMessages(String text);
 }
--- spring-data/src/main/java/be/ordina/spring/dao/MessageDaoImpl.java
+++ spring-data/src/main/java/be/ordina/spring/dao/MessageDaoImpl.java
@@ -3,6 +3,10 @@ package be.ordina.spring.dao;
 import be.ordina.spring.domain.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -10,6 +14,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author Ken Coenen
@@ -23,7 +28,23 @@ public class MessageDaoImpl implements MessageDao {
     private JdbcTemplate jdbcTemplate;
 
     @Override
+    public List<Message> getMessages() {
+        return jdbcTemplate.query("select * from message", new MessageMapper());
+    }
+
+    @Override
     public Message getMessage(Long id) {
+        return queryMessageWithJdbcTemplate(id);
+    }
+
+    private Message queryMessageWithJdbcTemplate(Long id) {
+        return jdbcTemplate.queryForObject("select * from message where id = ?", new Object[]{id}, new MessageMapper());
+    }
+
+    /**
+     * Already uses a {@link javax.sql.DataSource}, but still with plain JDBC after obtaining the connection.
+     */
+    private Message queryMessageWithJdbc(Long id) {
         Connection conn = null;
         try {
             // Don't ask a connection from the DriverManager, but from the already configured DataSource
@@ -50,7 +71,25 @@ public class MessageDaoImpl implements MessageDao {
     }
 
     @Override
+    public List<Message> searchMessages(String text) {
+        NamedParameterJdbcTemplate jdbcTemplate1 = new NamedParameterJdbcTemplate(ds);
+        SqlParameterSource params = new MapSqlParameterSource("text", text);
+        return jdbcTemplate1.query("select * from message where message like '%' || :text || '%'", params, new MessageMapper());
+    }
+
+    @Override
     public int getCount() {
         return jdbcTemplate.queryForObject("select count(*) from message", Integer.class);
+    }
+
+    /**
+     * Map each {@link java.sql.ResultSet} row into a {@link be.ordina.spring.domain.Message} object.
+     * Do this in a separate class so that we can reuse it.
+     */
+    class MessageMapper implements RowMapper<Message> {
+        @Override
+        public Message mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Message(rs.getLong("id"), rs.getString("message"), rs.getString("recipient"));
+        }
     }
 }
